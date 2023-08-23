2a5ff4c604eb1547cb9d864b96c64b6589648dfa
Keisuke-Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2023-08-23 12:15:31 +0900

    Add re.h

---
+++ re.h
@@ -0,0 +1,3 @@
+char* re_match(char* reg, char* str);
+char** re_search(char* reg, char* str);
+int re_replace(char* reg, char* repl, char* str);
--- tools.h
+++ tools.h
@@ -1,1 +1,8 @@
-int eq_reg(char* target, char* regular_expression);
+typedef struct {
+    char* key;
+    void* val;
+} dict;
+
+void* dict_get();
+
+int str_replace(char* repl, char* str);
